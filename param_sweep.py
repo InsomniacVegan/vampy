8cc709c9388ec435a58880ca9de3cbb5ab8cda56
InsomniacVegan <luke.e.elliott@gmail.com>
2020-03-04 13:35:28 +0000

    Param sweeper and base classes implemented

--- param_sweep.py
+++ param_sweep.py
@@ -26,6 +26,7 @@
 # ============================================================== #
 
 
+
 class ParametericSweeper:
     """Abstract parametric sweeper for use with VAMPIRE input files"""
     def __init__(self):
